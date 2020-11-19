2a25bbede30dd311ca6f42623e23c92bca4e6483
jinrunheng <1175088275@qq.com>
2020-11-19 14:27:19 +0800

    2020-11-19 打卡完成

---
+++ move-zeroes/Solution.go
@@ -0,0 +1,15 @@
+func moveZeroes(nums []int)  {
+    if nums == nil {
+        return
+    }
+    j := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != 0 {
+            nums[j] = nums[i]
+            j++
+        }
+    }
+    for i := j; i < len(nums); i++ {
+        nums[i] = 0;
+    }
+}
